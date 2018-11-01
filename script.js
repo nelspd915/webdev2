e779aa7c2688e63f34afbfc07e032a743cee965d
Nels Danielson <nelspd915@gmail.com>
2018-10-31 21:09:52 -0500

    Tried to load airports.csv
    Attempted to load airports.csv using omnivore, but isn't working yet. Also added a simple icon for the markers using airports.png

--- script.js
+++ script.js
@@ -34,7 +34,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // than just CSV files)
 // ref: https://github.com/mapbox/leaflet-omnivore
 
-
+omnivore.csv('airports.csv').addTo(map);
+// Not sure yet why the above line is not working.
 
 // FIXME: Add styling to markers
 // hint: refer to this tutorial for an example of how to do this:
@@ -43,12 +44,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var airportIcon = L.icon({
     iconUrl: 'airport.png',
-
-    /*iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor*/
 });
 
-//L.marker([40, 40], {icon: airportIcon}).addTo(map);
+// Not sure yet how to add this icon to the markers for the airports layer, and the airports layer isn't showing up yet anyway. 
+
+// Work in progress...
